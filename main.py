933c06ded3a4548c805aa5473041cac975eed1ee
eddy123 <edpren12@gmail.com>
2019-06-11 22:03:55 -0500

    Added about.html, home.html, and layout.html

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 from flask import Flask, request, redirect, render_template, session, flash
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql_pymysql://field-map:field-map@localhost:3306/field-map'
-#app.config['SQLALCHEMY_ECHO'] = True
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql_pymysql://field-map:field-map@localhost:3306/field-map'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
-#app.secret_key = 'secret_key'
+app.secret_key = 'secret_key'
+
+#list of dictionaries that we pull our data from in the html forms. 
+#will take the place of database for now
+posts = [
+    {
+        'author': 'Edward Prenzler',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 11, 2019'
+    },
+    {
+        'author': 'Ryan Theis',
+        'title': 'Blog Post 2',
+        'content': 'Next blog post',
+        'date_posted': 'June 11, 2019'
+    }
+]
 
 @app.route('/')
+@app.route('/map')
 def index():
-    #return redirect('/blog')
     return "Looks like this baby is up n running"
 
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
 
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
-app.run()
+if __name__ == '__main__':
+    app.run()
